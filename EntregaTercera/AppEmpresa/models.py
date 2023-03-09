54bdc2a36cdf180c16aaa13660596009cb920530
Ana Laura Insfran <analaura.insfran94@gmail.com>
2023-03-09 20:19:04 -0300

    Primer commit

---
+++ EntregaTercera/AppEmpresa/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+class Equipos(models.Model):
+
+    nombre = models.CharField(max_length=40)
+    camada = models.IntegerField()
+
+class Trabajador(models.Model):
+
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    email = models.EmailField()
+
+class ProjectManager(models.Model):
+
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    email = models.EmailField()
+    profesion = models.CharField(max_length=30)
+
+class Proyecto(models.Model):
+
+    nombre = models.CharField(max_length=30)
+    fechaentrega = models.DateField()
+    entregado = models.BooleanField()
