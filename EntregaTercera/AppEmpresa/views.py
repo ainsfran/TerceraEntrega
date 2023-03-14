8641d1842d8fac6a0eafa0870ffbcbe40391e4f2
Ana Laura Insfran <analaura.insfran94@gmail.com>
2023-03-13 23:55:45 -0300

    Segundo Commit

--- EntregaTercera/AppEmpresa/views.py
+++ EntregaTercera/AppEmpresa/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from AppEmpresa.models import Equipos
+from django.http import HttpResponse
 # Create your views here.
+
+def equipo(self):
+
+    equipo = Equipos(nombre = 'BIM', camada = '1')
+    equipo.save()
+    documentoDeTexto = f'Equipo: {equipo.nombre}    Camada: {equipo.camada}'
+
+    return HttpResponse(documentoDeTexto)
--- EntregaTercera/EntregaTercera/urls.py
+++ EntregaTercera/EntregaTercera/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from AppEmpresa.views import equipo
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('equipo/', equipo),
 ]
